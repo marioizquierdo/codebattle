e493c967179e875cc224a1b88351e4b85bda0e7a
Mario Izquierdo <tothemario@gmail.com>
2012-04-14 20:52:16 -0700

    Create Rails app and home page scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codebattle::Application.config.secret_token = 'cb669690bc49eac35746506bebf691c27b5eba265a8b74adf11c7b2cc5786966b78d2c4ba552134cba76d308bb9981e6fb2ada4a918d56d27e33989d2cbdc1e2'
